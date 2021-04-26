7818996034d508c027802e08bc72663a325faff6
loki <loki.yen@outlook.com>
2021-04-26 08:17:04 +0800

    feat: add Q1Solution

---
+++ backend/lab/leetcode/src/main/java/com/loki/lab/leetcode/Q2Solution.java
@@ -0,0 +1,24 @@
+package com.loki.lab.leetcode;
+
+public class Q2Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
