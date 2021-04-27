c8469809e4b153f05e488e053ac2d1e06738e01c
loki <loki.yen@outlook.com>
2021-04-27 08:11:20 +0800

    feat: add Q2Solution

--- backend/lab/leetcode/src/main/java/com/loki/lab/leetcode/Q2Solution.java
+++ backend/lab/leetcode/src/main/java/com/loki/lab/leetcode/Q2Solution.java
@@ -1,7 +1,12 @@
 package com.loki.lab.leetcode;
 
+import java.util.Optional;
+
+/**
+ * https://leetcode-cn.com/problems/add-two-numbers/
+ */
 public class Q2Solution {
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
@@ -19,6 +24,34 @@ public class Q2Solution {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        int carry = 0;
+        ListNode head = null;
+        ListNode tail = null;
+        while (l1 != null || l2 != null) {
+            int v1 = (l1 != null) ? l1.val : 0;
+            int v2 = (l2 != null) ? l2.val : 0;
+            int sum = v1 + v2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+
+
+        }
+
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+
+        return head;
     }
 }
