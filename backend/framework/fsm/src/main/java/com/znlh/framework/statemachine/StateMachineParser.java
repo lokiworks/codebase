d9e47e30eb38532e0d68a26825390d0ddc34e2ee
颜亚 <loki.yen@outlook.com>
2021-06-18 12:51:28 +0800

    feat: add config center support

--- backend/framework/fsm/src/main/java/com/znlh/framework/statemachine/StateMachineParser.java
+++ backend/framework/fsm/src/main/java/com/znlh/framework/statemachine/StateMachineParser.java
@@ -1,7 +1,5 @@
 package com.znlh.framework.statemachine;
 
-import com.znlh.framework.statemachine.StateMachine;
-
 public interface StateMachineParser {
     StateMachine parse(String machineId, String json);
 }
