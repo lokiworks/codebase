0fc6ac6be9b1dd6c4cf151f1b882a3f5d1017108
peter <loki.yen@outlook.com>
2023-06-03 19:42:16 +0800

    feat: add mobile login style.

--- frontend/antd/dashboard/myapp/src/pages/User/Login2/index.tsx
+++ frontend/antd/dashboard/myapp/src/pages/User/Login2/index.tsx
@@ -5,8 +5,11 @@ import { Alert, Tabs, message } from "antd";
 import Settings from '../../../../config/defaultSettings';
 import { useState } from "react";
 import { flushSync } from "react-dom";
-import { LoginForm, ProFormText } from "@ant-design/pro-components";
-import { AlipayCircleOutlined, LockOutlined, TaobaoCircleOutlined, UserOutlined, WeiboCircleOutlined } from "@ant-design/icons";
+import { DefaultFooter, LoginForm, ProFormCaptcha, ProFormCheckbox, ProFormText } from "@ant-design/pro-components";
+import { AlipayCircleOutlined, LockOutlined, MobileFilled, TaobaoCircleOutlined, UserOutlined, WeiboCircleOutlined } from "@ant-design/icons";
+import { size } from "lodash";
+import { getFakeCaptcha } from "@/services/ant-design-pro/login";
+import { Footer } from "antd/es/layout/layout";
 
 
 
@@ -80,6 +83,67 @@ const Login: React.FC = () => {
         }
     };
 
+    const Lang: React.FC = () => {
+        const langClassName = useEmotionCss(({ token }) => {
+            return {
+                width: '42px',
+                height: '42px',
+                lineHeight: '42px',
+                position: 'fixed',
+                right: 16,
+                borderRadius: token.borderRadius,
+                '&:hover': {
+                    color: token.colorPrimaryActive,
+                }
+            }
+        });
+
+        return (
+            <>
+                <AlipayCircleOutlined key="AlipayCircleOutlined" className={langClassName} />
+                <TaobaoCircleOutlined key="TaobaoCircleOutlined" className={langClassName} />
+                <WeiboCircleOutlined key="WeiboCircleOutlined" className={langClassName} />
+            </>
+        )
+    }
+
+    const Footer: React.FC = () => {
+        const intl = useIntl();
+        const defaultMessage = intl.formatMessage({
+            id: 'app.footer.message',
+            defaultMessage: '蚂蚁集团体验技术部出品'
+        });
+        const currentYear = new Date().getFullYear();
+        return (
+            <DefaultFooter
+                style={{
+                    background: 'none',
+                }}
+                links={[
+                    {
+                        key: 'Ant Design Pro',
+                        title: 'Ant Design Pro',
+                        href: 'https://pro.ant.design',
+                        blankTarget: true,
+                    },
+                    {
+                        key: 'github',
+                        title: <GithubOutlined />,
+                        href: 'https://github.com/ant-design/ant-design-pro',
+                        blankTarget: true,
+                    },
+                    {
+                        key: 'Ant Design',
+                        title: 'Ant Design',
+                        href: 'https://ant.design',
+                        blankTarget: true,
+                    },
+                ]}
+            />
+
+        );
+    };
+
     const handleSubmit = async (values: API.LoginParams) => {
         try {
             // 登录
@@ -122,6 +186,10 @@ const Login: React.FC = () => {
                 </title>
 
             </Helmet>
+            <Lang />
+
+            <div style={{ flex: 1 , padding :'32px 0'}}></div>
+
 
             <LoginForm
                 contentStyle={{
@@ -175,7 +243,8 @@ const Login: React.FC = () => {
                             defaultMessage: '账户或密码错误(admin/ant.design)'
                         })}
                         />
-                    )}
+                    )
+                }
 
 
                 {
@@ -215,14 +284,75 @@ const Login: React.FC = () => {
                     )
                 }
 
-                
+                {
+                    status === 'error' && loginType === 'mobile' && <LoginMessage content="验证码错误" />
+                }
+
+                {
+                    type === 'mobile' && (
+                        <>
+                            <ProFormText fieldProps={{ size: 'large', prefix: <MobileFilled />, }}
+                                name="mobile"
+                                placeholder={intl.formatMessage({ id: 'pages.login.phoneNumber.placeholder', defaultMessage: '手机号' })}
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: (<FormattedMessage id="pages.login.phoneNumber.required" defaultMessage="请输入手机号！" />),
+                                    },
+                                    {
+                                        pattern: /^1\d{10}$/,
+                                        message: (<FormattedMessage id="pages.login.phoneNumber.invalid" defaultMessage="手机号格式错误！" />),
+                                    },
+
+                                ]}
+                            />
+                            <ProFormCaptcha fieldProps={{ size: 'large', prefix: <LockOutlined />, }}
+                                captchaProps={{ size: 'large', }}
+                                placeholder={intl.formatMessage({ id: 'pages.login.captcha.placeholder', defaultMessage: '请输入验证码' })}
+                                captchaTextRender={(timing, count) => {
+                                    if (timing) {
+                                        return `${count} ${intl.formatMessage({ id: 'pages.getCaptchaSecondText', defaultMessage: '获取验证码' })}`;
+                                    }
+                                    return intl.formatMessage({ id: 'pages.login.phoneLogin.getVerificationCode', defaultMessage: '获取验证码' });
+                                }}
+                                name="captcha"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: (<FormattedMessage id="pages.login.captcha.required" defaultMessage="请输入验证码！" />),
+                                    }
+                                ]}
+                                onGetCaptcha={async (phone) => {
+                                    const result = await getFakeCaptcha({ phone });
+                                    if (!result) {
+                                        return;
+                                    }
+                                    message.success('获取验证码成功！验证码为：1234');
+                                }}
+                            />
+                        </>
+
+
+                    )
+                }
+
+                <div style={{ marginBottom: 24 }}>
+                    <ProFormCheckbox noStyle name="autoLogin">
+                        <FormattedMessage id="pages.login.rememberMe" defaultMessage="自动登录" />
+                    </ProFormCheckbox>
+                    <a style={{ float: 'right' }}>
+                        <FormattedMessage id="pages.login.forgotPassword" defaultMessage="忘记密码" />
+                    </a>
+
+
+                </div>
 
             </LoginForm>
-
         </div>
 
-    );
 
+
+    );
 
 };
 
